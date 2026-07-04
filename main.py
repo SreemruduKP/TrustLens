06d7b53600b804c4d34aa39486f1f8e5e5b589de
SreemruduKP <161940502+SreemruduKP@users.noreply.github.com>
2026-07-05 03:13:02 +0530

    connected to fast api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from urllib.parse import urlparse
+from predictor import predict_url
 
 app = FastAPI()
 
@@ -11,28 +11,9 @@ class URLInput(BaseModel):
 def home():
     return{"status":"running", "project":"TrustLens"}
 
-def extract_url_parts(netloc):
-    parts=netloc.split(".")
-    if len(parts) >=3:
-        subdomain = parts[0]
-        domain = ".".join(parts[1:])
-    else:
-        subdomain = None
-        domain = netloc
-    return subdomain,domain
-
 @app.post("/analyse-url")
 
 def analyse_url(data: URLInput):
-    parsed = urlparse(data.url)
-    subdomain, domain = extract_url_parts(parsed.netloc)
-    return{
-        "orginal_url" : data.url,
-        "scheme" : parsed.scheme,
-        "subdomain" : subdomain,
-        "domain" : domain,
-        "path" : parsed.path,
-        "query" : parsed.query,
-        "is_https" : parsed.scheme == "https",
-        "warning": "No HTTPS — connection not encrypted" if parsed.scheme != "https" else None
-        }
+    url = data.url
+    result = predict_url(url)
+    return result
